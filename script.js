0b393f281a561731d2ae7fb1e52818310e97e6be
AniruddhaMJois <aniruddhamanjunathajois1tab@gmail.com>
2026-09-17 22:37:11 +0530

    Connect registration form to Google Sheets backend

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     
     // NOTE: You need to create a Google Apps Script and deploy it as a Web App to get this URL.
     // Replace 'YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL' with your actual Google Script URL.
-    const scriptURL = 'YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL';
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbzuUiPD-JRnaLKaZ2z71e4wXM6c24pDRnP-Q-5XgnGCKP4b2MypQXAIuYW11EUljw_y1A/exec';
     
     if (scriptURL === 'YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL') {
         messageEl.textContent = 'Please configure the Google Apps Script URL in script.js to submit to a spreadsheet.';
